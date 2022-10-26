7c1f2a0c88c3c8a3180d2f19528b187612e738c2
Adam <adamszydlo@gmail.com>
2022-10-26 23:46:41 +0200

    Changed Region to EU

--- examples/eks-cluster-with-new-vpc/main.tf
+++ examples/eks-cluster-with-new-vpc/main.tf
@@ -26,14 +26,14 @@ locals {
   name = basename(path.cwd)
   # var.cluster_name is for Terratest
   cluster_name = coalesce(var.cluster_name, local.name)
-  region       = "us-west-2"
+  region       = "eu-central-1"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
     Blueprint  = local.name
-    GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
+    GithubRepo = "https://github.com/adsz/terraform-aws-eks-blueprints"
   }
 }
 
